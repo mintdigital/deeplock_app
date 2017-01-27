6fdf7d5e325c1389f444e9e1c47b5c93b64e899c
Ed Ellson <eellson@gmail.com>
2017-01-27 14:08:23 +0000

    Try new release

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DeeplockApp.Mixfile do
 
   def project do
     [app: :deeplock_app,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
