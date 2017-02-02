7e81ebffff950e4ac87fe3a0e95e4385ade51e76
Ed Ellson <eellson@gmail.com>
2017-02-02 15:54:10 +0000

    Adds comments to compilation script

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,8 +2,11 @@
 
 cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 
+### Update these values for your own S3 bucket ###
 SECRETS_S3_BUCKET=deeplock-app-production-secrets
 S3_BUCKET_REGION=eu-west-1
+### Don't update below this line ###
+
 VERSION=$(grep version mix.exs | sed 's/^.*version: "//' | sed 's/",//')
 APP_NAME=$(grep app: mix.exs | sed 's/^.*app: ://' | sed 's/,//')
 
@@ -12,14 +15,19 @@ aws s3 cp s3://$SECRETS_S3_BUCKET/creds.txt --region $S3_BUCKET_REGION creds.txt
 eval $(cat creds.txt | sed 's/^/export /')
 rm creds.txt
 
+# Install deps
 mix local.hex --force
 mix local.rebar --force
 mix deps.get
 npm install
+
+# Build assets and release
 ./node_modules/brunch/bin/brunch build --production
 MIX_ENV=prod mix phoenix.digest
 MIX_ENV=prod mix release --env=prod
 echo $?
+
+# Move release into run location, allow execute by owner
 mkdir -p /var/app/current
 chown owner /var/app/current
 cp _build/prod/rel/$APP_NAME/releases/$VERSION/$APP_NAME.tar.gz /var/app/current/$APP_NAME.tar.gz
